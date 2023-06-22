d0974c1bac752b46e0a2ec00ec800b7f2ca7e06c
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-22 09:16:57 +0200

    Add basic structrure, create react app

---
+++ client/to_do_list/src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import App from './App';
+
+
+const container = document.querySelector('#root')
+const root = createRoot(container)
+
+root.render(
+  <App />
+);
