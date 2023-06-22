da33b9d914022b6336bdd92b462411e1f4635ae7
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-22 10:06:17 +0200

    Add style, ids in html

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <header className="app-title">
         <h1>My to do list</h1>
       </header>
-      <section className="tasks-section">
+      <section className="tasks-section" id="tasks-section">
         <h2>Tasks</h2>
-        <ul className="tasks-list">
-          <li className="task">Wash dishes <button className="btn-red">Remove</button></li>
-          <li className="task">Do the laundry <button className="btn-red">Remove</button></li>
-          <li className="task">Take out the trash <button className="btn-red">Remove</button></li>
+        <ul className="tasks-list" id="tasks-list">
+          <li className="task">Wash dishes <button className="btn btn-red">Remove</button></li>
+          <li className="task">Do the laundry <button className="btn btn-red">Remove</button></li>
+          <li className="task">Take out the trash <button className="btn btn-red">Remove</button></li>
         </ul>
-        <form>
-          <input className="text-input" type="text" placeholder="add task"></input>
+        <form id="add-task-form">
+          <input className="text-input" type="text" placeholder="add task" id="task-name"></input>
           <button className="btn" type="submit">Add task</button>
         </form>
       </section>
