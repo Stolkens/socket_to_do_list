6590a92f79c20ed6251ef5e5fc87fe348a8a0574
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-22 09:44:43 +0200

    Add htm structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,21 @@
 const App = () => {
   return (
     <div className="App">
-      Hello world
+      <header className="app-title">
+        <h1>My to do list</h1>
+      </header>
+      <section className="tasks-section">
+        <h2>Tasks</h2>
+        <ul className="tasks-list">
+          <li className="task">Wash dishes <button className="btn-red">Remove</button></li>
+          <li className="task">Do the laundry <button className="btn-red">Remove</button></li>
+          <li className="task">Take out the trash <button className="btn-red">Remove</button></li>
+        </ul>
+        <form>
+          <input className="text-input" type="text" placeholder="add task"></input>
+          <button className="btn" type="submit">Add task</button>
+        </form>
+      </section>
     </div>
   );
 }
